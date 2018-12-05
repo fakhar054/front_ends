455de180e129dbadfdd6042eb3270a0c9a6dd169
fakhar054 <fakharali054@gmail.com>
2018-12-05 15:48:24 +0000

    android

--- app/src/main/java/com/example/thinkgeniux/front_ends/Forget_Password.java
+++ app/src/main/java/com/example/thinkgeniux/front_ends/Forget_Password.java
@@ -9,6 +9,5 @@ public class Forget_Password extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget__password);
-        getSupportActionBar().hide();
     }
 }
--- app/src/main/java/com/example/thinkgeniux/front_ends/MainActivity.java
+++ app/src/main/java/com/example/thinkgeniux/front_ends/MainActivity.java
@@ -1,26 +1,39 @@
 package com.example.thinkgeniux.front_ends;
 
 import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView reg,forget;
     Button login;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().hide();
+
 
 
         reg=findViewById(R.id.reg);
+        imageView=findViewById(R.id.imageView);
         login=findViewById(R.id.login);
         forget=findViewById(R.id.forget);
+
+        Bitmap bitmap= BitmapFactory.decodeResource(getResources(),R.drawable.hair);
+        RoundedBitmapDrawable roundedBitmapDrawable= RoundedBitmapDrawableFactory.create(getResources(),bitmap);
+        roundedBitmapDrawable.setCircular(true);
+        imageView.setImageDrawable(roundedBitmapDrawable);
 
      forget.setOnClickListener(new View.OnClickListener() {
             @Override
